a53899b7ba5f0f499455eee7fa6e8ab5e93b0f64
Eri-Murakami <toypoodle.trumpet.35978@outlook.jp>
2024-05-01 21:52:43 +0900

    2024.5.1

--- insert.php
+++ insert.php
@@ -13,19 +13,17 @@ $pdo->exec("insert into contactform(name,mail,age,comments)values
 <head>
 
     <meta charset = "utf-8">
-    <title>お問い合わせフォームを作る</title>
+    <title>アカウント登録完了画面</title>
     <link rel = "stylesheet"type = "text/css" href = "01_PHP%2016%E6%99%82%E9%96%93%E7%9B%AE(%E6%BC%94%E7%BF%921%20%E5%95%8F%E5%90%88%E3%82%8F%E3%81%9B%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A0%E3%81%AE%E4%BD%9C%E6%88%90%EF%BC%89%E4%BF%AE%202%20style.css">
 
 </head>
     
 <body>
-    <h1>お問い合わせフォーム</h1>
+    <h1>登録完了しました</h1>
     
-    <div class = "confirm">
-        <p>お問い合わせありがとうございました。<br>３営業以内に担当よりご連絡差し上げます。
-        </p>
-    
-    </div>
+      <div>
+                <input type="submit"class="submit"value="TOPページへ戻る">
+            </div>
 </body>
 
 </html>
--- return index.php
+++ return index.php
@@ -14,19 +14,49 @@
         <form method="post"action="mail_confirm.php"method="post">
            
             <div>
-                <label>名前</label><br>
-                <input type="text"class="text"size="35"name="name"
+                <label>名前（性）</label><br><!--　最大10文字　/　初期値空白　/　ひらがな、漢字のみ　-->
+                <input type="text"class="text"size="35"name="last name"maxlength="10"pattern="^[ぁ-ん一-龠]*$"
                        value="<?php 
-                              if(!empty($_POST['name'])){
-                                  echo $_POST['name'];
+                              if(!empty($_POST['last name'])){
+                                  echo $_POST['last name'];
                               }
                               ?>">
             </div>
             
             <div>
-                <label>メールアドレス</label>
-                <br>
-                <input type="text"class="text"size="35"name="mail"
+                <label>名前（名）</label><br><!--　最大10文字　/　初期値空白　/　ひらがな、漢字のみ　-->
+                <input type="text"class="text"size="35"name="first name"maxlength="10"pattern="^[ぁ-ん一-龠]*$"
+                       value="<?php 
+                              if(!empty($_POST['first name'])){
+                                  echo $_POST['first name'];
+                              }
+                              ?>">
+            </div>
+            
+            <div>
+                <label>カナ（性）</label><br><!--　最大10文字　/　初期値空白　/　カタカナのみ　-->
+                <input type="text"class="text"size="35"name="last name2"maxlength="10"pattern="^[ァ-ヶｱ-ﾝﾞﾟ]+$"
+                       value="<?php 
+                              if(!empty($_POST['last name2'])){
+                                  echo $_POST['last name2'];
+                              }
+                              ?>">
+            </div>
+            
+             <div>
+                <label>カナ（性）</label><br><!--　最大10文字　/　初期値空白　/　カタカナのみ　-->
+                <input type="text"class="text"size="35"name="first name2"maxlength="10"pattern="^[ァ-ヶｱ-ﾝﾞﾟ]+$"
+                       value="<?php 
+                              if(!empty($_POST['first name2'])){
+                                  echo $_POST['first name2'];
+                              }
+                              ?>">
+            </div>
+            
+            <div>
+                <label>メールアドレス</label><br> 
+                <!--　最大10文字　/　初期値空白　/　半角英数字、半角ハイフン、半角記号(ハイフンとアットマーク)のみ　※「.」いらない？-->
+                <input type="mail"class="text"size="35"name="mail"maxlength="100"pattern="^[0-9a-zA-Z\-\@]*$">
                        value="<?php 
                               if(!empty($_POST['mail'])){
                                   echo $_POST['mail'];
@@ -34,6 +64,18 @@
                               ?>">
             </div>
             
+             <div>
+                <label>パスワード</label><br><!--　最大10文字　/　初期値空白　/　半角英数字　-->
+                <input type="password"class="text"size="10"name="password"maxlength="10"pattern="^[a-zA-Z-9]+$^">
+                       value="<?php 
+                              if(!empty($_POST['password'])){
+                                  echo $_POST['password'];
+                              }
+                              ?>">
+            </div>
+            
+    
+         <!--
             <div>
                 <label>年齢</label>
                 <br>
@@ -41,8 +83,8 @@
                     <option>選択してください</option>
                     <?php
                         $age = 0;
-                        if(!empty($_POST['age'])) {
-                            $age = $_POST['age'];
+                        if(!empty($_POST['sex'])) {
+                            $age = $_POST['sex'];
                         }
                         for ($i = 18; $i <=65; $i++) {
                             if ($i == $age) {
@@ -53,18 +95,39 @@
                         }
                     ?>
                 </select>
+            </div>-->
+            -->
+            
+            <div>
+                <label>郵便番号</label><br><!--　最大7文字　/　初期値空白　/　半角数字のみ-->
+                <input type="text"size="10"name="post code"maxlength="7"pattern="[0-9]+">
+                       value="<?php 
+                              if(!empty($_POST['post code'])){
+                                  echo $_POST['post code'];
+                              }
+                              ?>">
             </div>
             
             <div>
-                <label>コメント</label>
-                <br>
-                <textarea cols="35"rows="7"name="comments"><?php 
-                    if(!empty($_POST['comments'])){
-                        echo $_POST['comments'];
-                    }
-                    ?>
-                s
-                </textarea>
+                <label>住所（市区町村）</label><br>
+                 <!--　最大10文字　/　初期値空白　/　ひらがな、漢字、数字、カタカナ、記号(ハイフンとアットマーク)のみ　-->
+                <input type="text"　class="text"size="35"　name="municipalities" maxlength="10" pattern="^[ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠\-\s\0-9]*$">
+                       value="<?php 
+                              if(!empty($_POST['municipalities'])){
+                                  echo $_POST['municipalities'];
+                              }
+                              ?>">
+            </div>
+            
+             <div>
+                <label>住所（市区町村）</label><br>
+                 <!--　最大10文字　/　初期値空白　/　ひらがな、漢字、数字、カタカナ、記号(ハイフンとアットマーク)のみ　-->
+                <input type="text"　class="text"size="35"　name="municipalities" maxlength="10" pattern="^[ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠\-\s\0-9]*$">
+                       value="<?php 
+                              if(!empty($_POST['municipalities'])){
+                                  echo $_POST['municipalities'];
+                              }
+                              ?>">
             </div>
             
             <div>
